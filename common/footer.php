676bb1ba083b04b2e0b6e65c73f22d091a02d94e
hiro <takehiro.ishio77@gmail.com>
2017-06-05 15:23:19 +0000

    set timezone

--- common/footer.php
+++ common/footer.php
@@ -1,7 +1,15 @@
         <div id="footer">
-                <small><p>Copyright &copy; 2017 PHP-keijiban.co.jp All Rights Reserved.</p></small>
+            <ul>
+                <li><a href="#">使い方ガイド</a></li>  
+                <li><a href="#">よくあるご質問</a></li>
+                <li><a href="#">利用規約</a></li>
+                <li><a href="#">お問い合わせ</a></li>
+                <li><a href="#">個人情報保護方針</a></li>
+                <li><a href="#">運営会社</a></li>
+                <li><a href="#">サイトマップ</a></li>
+            </ul>
+            <small><p>Copyright &copy; 2017 PHP-keijiban.co.jp All Rights Reserved.</p></small>
         </div> 
-        
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
         <script type="text/javascript" src="js/main.js"></script>
     </body>
